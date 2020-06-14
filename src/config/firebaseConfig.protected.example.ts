de5d6942e22b644c0ba6988052a9eb3bff1b4074
Hentuloo <chentulooo@gmail.com>
2020-06-14 12:12:36 +0200

    remove protected api key

---
+++ src/config/firebaseConfig.protected.example.ts
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'AIzaSyDhLqGd_9MZMQOF76Tr7TM2W9rVFUnYI2s',
+  authDomain: 'typing-page.firebaseapp.com',
+  databaseURL: 'https://typing-page.firebaseio.com',
+  projectId: 'typing-page',
+  storageBucket: 'typing-page.appspot.com',
+  messagingSenderId: '980265800534',
+  appId: '1:980265800534:web:1a82d2315ceb21a0d17cf7',
+  measurementId: 'G-LXZ47TJVY8',
+};
--- src/fb/index.tsx
+++ src/fb/index.tsx
@@ -5,17 +5,7 @@ import 'firebase/analytics';
 import 'firebase/auth';
 import 'firebase/functions';
 import 'firebase/database';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDhLqGd_9MZMQOF76Tr7TM2W9rVFUnYI2s',
-  authDomain: 'typing-page.firebaseapp.com',
-  databaseURL: 'https://typing-page.firebaseio.com',
-  projectId: 'typing-page',
-  storageBucket: 'typing-page.appspot.com',
-  messagingSenderId: '980265800534',
-  appId: '1:980265800534:web:1a82d2315ceb21a0d17cf7',
-  measurementId: 'G-LXZ47TJVY8',
-};
+import firebaseConfig from 'config/firebaseConfig.protected';
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
