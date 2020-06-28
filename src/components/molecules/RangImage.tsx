283a7acbb730dd8c34f9d2e85f5830d84a27606b
Hentuloo <chentulooo@gmail.com>
2020-06-28 17:35:30 +0200

    correct rand image (display image by level)

--- src/components/molecules/RangImage.tsx
+++ src/components/molecules/RangImage.tsx
@@ -1,8 +1,12 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import styled from 'styled-components';
 
 import level1 from 'assets/svg/levels/level1.svg';
+import level2 from 'assets/svg/levels/level2.svg';
+import level3 from 'assets/svg/levels/level3.svg';
 import Tippy from '@tippyjs/react';
+import { useSelector } from 'react-redux';
+import { getSoloTrainingLevel } from 'store/selectors/soloTraining.selector';
 
 const Wrapper = styled.div`
   position: relative;
@@ -16,10 +20,19 @@ const Image = styled.img`
 `;
 
 export const RangImage: FC = ({ ...props }) => {
+  const level = useSelector(getSoloTrainingLevel);
+
+  const levelImage = useMemo(() => {
+    if (!level) return;
+    if (level > 34) return level3;
+    if (level > 22) return level2;
+    return level1;
+  }, [level]);
+
   return (
     <Tippy content="Ranga rozgrywek solowych">
       <Wrapper {...props}>
-        <Image src={level1} alt="Ranga" />
+        <Image src={levelImage} alt="Ranga" />
       </Wrapper>
     </Tippy>
   );
--- src/store/selectors/soloTraining.selector.ts
+++ src/store/selectors/soloTraining.selector.ts
@@ -1,4 +1,10 @@
 import { StoreType } from 'store/store';
+import { createSelector } from 'reselect';
 
 export const getSoloTraining = (store: StoreType) =>
   store.soloTraining;
+
+export const getSoloTrainingLevel = createSelector(
+  getSoloTraining,
+  ({ level }) => level,
+);
